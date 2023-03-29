285bc86241dfe9ba3cd20630c39b21dd631c2d46
JP-Becker <jpbecker20@gmail.com>
2023-03-29 11:04:30 -0300

    tabela posts

--- labeddit.sql
+++ labeddit.sql
@@ -1,5 +1,5 @@
 -- Active: 1679754740218@@127.0.0.1@3306
-CREATE TABLE USERS (
+CREATE TABLE users (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL,
     email TEXT UNIQUE NOT NULL,
@@ -8,10 +8,21 @@ CREATE TABLE USERS (
     created_at TEXT DEFAULT(DATETIME()) NOT NULL
 );
 
+CREATE TABLE posts (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    creator_id TEXT NOT NULL,
+    comments INTEGER DEFAULT(0) NOT NULL,
+    likes INTEGER DEFAULT(0) NOT NULL,
+    dislikes INTEGER DEFAULT(0) NOT NULL,
+    created_at TEXT DEFAULT(DATETIME()) NOT NULL,
+    uploaded_at TEXT DEFAULT(DATETIME()) NOT NULL,
+    FOREIGN KEY (creator_id) REFERENCES users (id)
+);
+
 INSERT INTO users (id, name, email, password, role)
 VALUES 
     ("u001", "Joao", "jp@gmail.com", "jp12345", "admin"),
     ("u002", "Maria", "maria@gmail.com", "maria12345", "normal"),
     ("u003", "Haroldo", "haroldo@gmail.com", "haroldo12345", "normal"),
-    ("u004", "Josiscleide", "josiscleide@gmail.com", "josiscleide12345", "normal"),
-    ("u005", "Paula", "paula@gmail.com", "paula12345", "normal");
+
+INSERT INTO 
